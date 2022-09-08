2c6db2b42e5fa53ec0e9623a1b0c79e725eaa297
GitHub Action <action@github.com>
2022-09-08 21:45:39 +0000

    Add changes for 887a4025dc5ff50e99428a9b28697aa66107fd8f

--- README.md
+++
@@ -1,1 +0,0 @@
-# libnest
--- libnest/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# from libnest.units import *
-# from libnest.definitions import *
-# from libnest.io import *
-# from libnest.bsk import *
-
-__version__ = "2022.04"
-__author__  = "Daniel Pęcak <daniel.pecak@gmail.com>"
